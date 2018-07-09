bbb1fde9faa09d42200098ad835d4a5738dedd85
Natalya Karamysheva <n.v.karamysheva@gmail.com>
2018-07-09 16:31:29 +0600

    Changed application purpose to flush unsafe_volume_music_active_ms to zero.

--- app/src/main/java/com/nata/volumecontrol/MainActivity.java
+++ app/src/main/java/com/nata/volumecontrol/MainActivity.java
@@ -1,86 +1,74 @@
 package com.nata.volumecontrol;
 
+import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.concurrent.TimeUnit;
 
-    private static final String AUDIO_SAFE_VOLUME_STATE = "audio_safe_volume_state";
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int UNSAFE_VOLUME_MUSIC_ACTIVE_MS_MAX = (20 * 3600 * 1000); // 20 hours
     private static final String UNSAFE_VOLUME_MUSIC_ACTIVE_MS = "unsafe_volume_music_active_ms";
 
-    private Button btnGetCurrent;
-    private TextView tvCurrentState;
-    private Button btnUpdateCurrent;
-    private EditText etNewState;
+    private ProgressBar progressCurrent;
+    private Button btnFlush;
+    private TextView tvTotalPlayed;
+    private TextView tvMaxDuration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnGetCurrent = findViewById(R.id.btnGetCurrentValue);
-        btnUpdateCurrent = findViewById(R.id.btnUpdateCurrentValue);
-        tvCurrentState = findViewById(R.id.tvCurrentState);
-        etNewState = findViewById(R.id.etNewState);
+        progressCurrent = findViewById(R.id.progress_current);
+        btnFlush = findViewById(R.id.btn_flush);
+        tvTotalPlayed = findViewById(R.id.tv_total_played);
+        tvMaxDuration = findViewById(R.id.tv_max_duration);
+        tvMaxDuration.setText(durationToText(UNSAFE_VOLUME_MUSIC_ACTIVE_MS_MAX));
 
+        updateProgressBar();
+
+        btnFlush.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                putTotalUnsafeMilliseconds(1);
+                updateProgressBar();
+            }
+        });
+    }
+
+    private void updateProgressBar() {
         try {
-            int currentState = getCurrentValue();
-            tvCurrentState.setText(Integer.toString(currentState));
+            int currentState = getUnsafeMilliseconds();
+            tvTotalPlayed.setText(durationToText(currentState));
+            progressCurrent.setMax(UNSAFE_VOLUME_MUSIC_ACTIVE_MS_MAX);
+            progressCurrent.setProgress(currentState);
+
         } catch (Settings.SettingNotFoundException e) {
-            Toast.makeText(this, "Could not load setting: " + e.getMessage(), Toast.LENGTH_LONG);
+            Log.e(TAG, "Could not load setting", e);
+            Toast.makeText(this,"Could not load current value.", Toast.LENGTH_SHORT).show();
         }
-
-        btnGetCurrent.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try {
-                    int currentState = getCurrentValue();
-                    tvCurrentState.setText(Integer.toString(currentState));
-                } catch (Settings.SettingNotFoundException e) {
-                    Toast.makeText(MainActivity.this, "Could not load setting: " + e.getMessage(), Toast.LENGTH_LONG);
-                }
-            }
-        });
-
-        btnUpdateCurrent.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                putValue(Integer.parseInt(MainActivity.this.etNewState.getText().toString()));
-            }
-        });
     }
 
-    private void putValue(int value) {
-        //putAudioSafe(value);
-        putTotalUnsafeMilliseconds(value);
-    }
-
-    private int getCurrentValue() throws Settings.SettingNotFoundException {
-        //return getAudioSafe();
-        return getUnsafeMIlliseconds();
-    }
-
-    private int getAudioSafe() throws Settings.SettingNotFoundException {
-        return Settings.Global.getInt(getContentResolver(), AUDIO_SAFE_VOLUME_STATE);
-    }
-
-    private void putAudioSafe(int value) {
-        Settings.Global.putInt(getContentResolver(),
-                AUDIO_SAFE_VOLUME_STATE,
-                value);
+    private int getUnsafeMilliseconds() throws Settings.SettingNotFoundException {
+        return Settings.Secure.getInt(getContentResolver(), UNSAFE_VOLUME_MUSIC_ACTIVE_MS);
     }
 
     private void putTotalUnsafeMilliseconds(int value) {
         Settings.Secure.putInt(getContentResolver(), UNSAFE_VOLUME_MUSIC_ACTIVE_MS, value);
     }
 
-    private int getUnsafeMIlliseconds() throws Settings.SettingNotFoundException {
-        return Settings.Secure.getInt(getContentResolver(), UNSAFE_VOLUME_MUSIC_ACTIVE_MS);
+    private String durationToText(int duration) {
+        long hours = TimeUnit.MILLISECONDS.toHours(duration);
+        long minutes = TimeUnit.MILLISECONDS.toMinutes(duration) - hours * 60;
+
+        return String.format("%02d:%02d", hours, minutes);
     }
 }
