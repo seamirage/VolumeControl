106edbe9274dfea2357a5cb15b1a0147b88d84f0
Natalya Karamysheva <n.v.karamysheva@gmail.com>
2020-03-04 15:00:22 +0600

    Converted to Kotlin project, migrated to AndroidX.

--- app/src/main/java/com/trueapps/volumecontrol/common/ui/NumberPickerDialog.java
+++ app/src/main/java/com/trueapps/volumecontrol/common/ui/NumberPickerDialog.java
@@ -2,7 +2,7 @@ package com.trueapps.volumecontrol.common.ui;
 
 import android.content.Context;
 import android.content.DialogInterface;
-import android.support.v7.app.AlertDialog;
+import androidx.appcompat.app.AlertDialog;
 import android.view.Gravity;
 import android.widget.FrameLayout;
 import android.widget.NumberPicker;
--- app/src/main/java/com/trueapps/volumecontrol/reminder/VolumeControlReminderService.java
+++ app/src/main/java/com/trueapps/volumecontrol/reminder/VolumeControlReminderService.java
@@ -5,10 +5,9 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.media.AudioManager;
 import android.os.Build;
 import android.provider.Settings;
-import android.support.v4.app.NotificationCompat;
+import androidx.core.app.NotificationCompat;
 import android.util.Log;
 
 import com.firebase.jobdispatcher.JobParameters;
